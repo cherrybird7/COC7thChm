9d6988f0409d870af894d02256726ccf247def6f
凤吹风雪 <charryfan@foxmail.com>
2024-12-01 01:31:20 +0800

    1

--- 模板/快速建立文件夹.bat
+++ 模板/快速建立文件夹.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem 设置目录
-set "base_dir=你新设置的文件的路径"
+set "base_dir=C:\Users\Administrator\Desktop\1"
 
 rem 创建字母 A 到 Z 的文件夹
 for %%D in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
