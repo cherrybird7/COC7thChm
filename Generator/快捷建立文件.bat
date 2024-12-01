dd1b134b3badca5e8bc59f9e50ce7b0609328f81
凤吹风雪 <charryfan@foxmail.com>
2024-12-01 23:17:55 +0800

    1

--- Generator/快捷建立文件.bat
+++ Generator/快捷建立文件.bat
@@ -6,7 +6,7 @@ set "templatePath=C:\Users\Administrator\Desktop\全白页.htm"
 rem 设置目标文件夹
 set "outputFolder=C:\Users\Administrator\Desktop\1"
 
-rem 创建输出文件并写入模板内容
+rem 创建输出文件并写入模板内容，将要创建的文件名贴近下方的括号中
 for %%f in (文件1 文件2 文件3 文件4 文件5) do (
     set "fileName=%%f.htm"
     copy /y "!templatePath!" "!outputFolder!\!fileName!"
